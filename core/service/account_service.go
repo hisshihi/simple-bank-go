807a5a0b072ba33fd4600b92c87b9c63db591032
hisshihi <hissoffc@gmail.com>
2025-12-11 00:43:43 +0500

    Добавлен метод Create в AccountRepo для создания аккаунта с указанием владельца, баланса и валюты

--- core/service/account_service.go
+++ core/service/account_service.go
@@ -1,7 +1,18 @@
 // Package service сервисный слой для работы с аккаунтами
 package service
 
+import (
+	"context"
+
+	"github.com/hisshihi/simple-bank/core/db"
+)
+
 type AccountRepository interface {
+	Create(ctx context.Context, owner string, balance float64, currency string) error
+	FindByID(ctx context.Context, id uint) (*db.Account, error)
+	FindAllAccounts(ctx context.Context) ([]db.Account, error)
+	UpdateBalance(ctx context.Context, id uint, balance float64) error
+	Delete(ctx context.Context, id uint) error
 }
 
 type AccountService struct {
